e4f5751d9223b7e9f7e1227278ddcfb98220b474
Oskar Legner <oskarlegner@gmail.com>
2024-10-05 14:21:43 +0200

    Add Map class with Draw method

---
+++ GooseJump/Goose.cs
@@ -0,0 +1,18 @@
+﻿
+namespace GooseJump
+{
+
+
+    internal class Map()
+    {
+        public int Width { get; }  = Console.WindowWidth;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight/2);
+
+            for (int i = 0; i < Width; i++)
+                Console.Write("-");
+        }
+    }
+}
