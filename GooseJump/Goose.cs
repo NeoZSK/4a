776d914db12e03ada8159be082acdd589e935305
Oskar Legner <oskarlegner@gmail.com>
2024-10-05 14:22:54 +0200

    Add Goos class

--- GooseJump/Goose.cs
+++ GooseJump/Goose.cs
@@ -1,7 +1,24 @@
 ﻿
 namespace GooseJump
 {
+    internal class Goose(int posX)
+    {
+        public int PosY { get; private set; } = 0;
+        public int PosX { get; } = posX;
 
+        private int VelocityY { get; set; } = 0;
+
+        public void Draw()
+        {
+            var tempPos = Console.GetCursorPosition();
+            Console.SetCursorPosition(PosX, Console.WindowHeight / 2 - 1 - PosY);
+            Console.Write("*");
+            Console.SetCursorPosition(tempPos.Left, tempPos.Top);
+        }
+
+
+
+    }
 
     internal class Map()
     {
