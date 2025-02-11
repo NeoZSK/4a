7f8ae5e1d0d2b747df5f17ae6240d82fc3c9840f
Oskar <oskar.legner@zsk.poznan.pl>
2025-02-11 15:37:04 +0100

    Add oop basics code

---
+++ OOP-Basics/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace OOP_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chicken c = new Chicken();
+            c.Name = "Czarek";
+            c.Weight = new Weight(14, "kg");
+            c.Jump();
+            c.SetAge(-100);
+   
+        }
+    }
+
+    public class Chicken
+    {
+        public string Name;
+        private int Age;
+        public Weight Weight;
+
+
+        public void SetAge(int age)
+        {
+            if (age > 0 && age < 100)
+            {
+                Age = age;
+            }
+            else
+            {
+                throw new Exception("Age must be between 0 and 100");
+            }
+
+        }
+        public void Jump()
+        {
+            Console.WriteLine($"{Name} jumps");
+        }
+    }
+
+    public class Weight
+    {
+        public int Value;
+        public string Type;
+
+        public Weight(int value, string type)
+        {
+            Value = value;
+            Type = type;
+        }
+    }
+
+}
