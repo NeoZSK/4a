e7ea43a74dfea16f00f66b85d7c9bfa805b9b69a
Oskar <oskar.legner@zsk.poznan.pl>
2025-03-04 13:53:11 +0100

    Refactor RPG to use OOP

---
+++ RPG/Map.cs
@@ -0,0 +1,63 @@
+﻿
+namespace RPG
+{
+
+    internal class Map
+    {
+        public const char WALL = '#';
+        public const char GROUND = '.';
+
+        private const string ROOT_DATA_PATH = @"C:\Users\User\Documents\data";
+        private const string MAP_PATH = ROOT_DATA_PATH + @"\map.txt";
+
+        private char[,] _board;
+
+        public void Draw( Position playerPos)
+        {
+            for (int j = 0; j < _board.GetLength(1); j++)
+            {
+                for (int i = 0; i < _board.GetLength(0); i++)
+                {
+                    if (i == playerPos.X && j == playerPos.Y)
+                    {
+                        Console.Write(Player.ICON);
+                    }
+                    else
+                    {
+                        Console.Write(_board[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ImportBoard()
+        {
+            string[] lines = File.ReadAllLines(MAP_PATH);
+            int height = lines.Length;
+            int width = lines[0].Length;
+            _board = new char[width, height];
+            for (int i = 0; i < height; i++)
+            {
+                char[] linia = lines[i].ToCharArray();
+                for (int j = 0; j < width; j++)
+                {
+                    _board[j, i] = linia[j];
+                }
+            }
+        }
+
+        public char GetBoardTile(Position pos)
+        {
+            if (pos.X < 0 || pos.X >= _board.GetLength(0)) return WALL;
+            if (pos.Y < 0 || pos.Y >= _board.GetLength(1)) return WALL;
+            return _board[pos.X, pos.Y];
+        }
+
+        public bool IsSteppable(char tile)
+        {
+            char[] steppableTiles = { GROUND };
+            return steppableTiles.Contains(tile);
+        }
+    }
+}
--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,110 +1,23 @@
-﻿const string ROOT_DATA_PATH = @"C:\Users\User\Documents\data";
-const string MAP_PATH = ROOT_DATA_PATH + @"\map.txt";
+﻿using RPG;
 
-const char PLAYER = '*';
-const char WALL = '#';
-const char GROUND = '.';
 
-Position playerPos = new Position()
-{
-    X = 0,
-    Y = 0
-};
-List<string> playerEq = new List<string>();   
-char[,] map;
+Map map = new Map();
+map.ImportBoard();
 
-ImportBoard(out map);
+Player p = new Player("Johny", map);
+Console.WriteLine(p.Name);
 
 while (true)
 {
     Console.Clear();
-    Draw(map, playerPos);
-    Move(map, ref playerPos);
+    map.Draw(p.position);
+    p.Move();
 }
 
 
-void Move(char[,] board, ref Position playerPos)
-{
-    var key = Console.ReadKey();
-    Position nextPos = playerPos;
 
-    switch (key.Key)
-    {
-        case ConsoleKey.UpArrow:
-            nextPos.Y -= 1;
-            break;
-        case ConsoleKey.DownArrow:
-            nextPos.Y += 1;
-            break;
-        case ConsoleKey.LeftArrow:
-            nextPos.X -= 1;
-            break;
-        case ConsoleKey.RightArrow:
-            nextPos.X += 1;
-            break;
-    }
 
-    UpdatePosition(ref playerPos, nextPos, board)
-}
 
-void UpdatePosition(ref Position entityPos, Position nextPos, char[,] board)
-{
-    char tile = GetBoardTile(board, nextPos);
-    if (IsSteppable(tile))
-    {
-        entityPos = nextPos;
-    }
-}
 
-char GetBoardTile(char[,] board, Position pos)
-{
-    if (pos.X < 0 || pos.X >= board.GetLength(0)) return WALL;
-    if (pos.Y < 0 || pos.Y >= board.GetLength(1)) return WALL;
-    return board[pos.X, pos.Y];
-}
 
-bool IsSteppable(char tile)
-{
-    char[] steppableTiles = { GROUND,  };
-    return steppableTiles.Contains(tile);
-}
-void Draw(char[,] map, Position playerPos)
-{
-    for (int j = 0; j < map.GetLength(1); j++)
-    {
-        for (int i = 0; i < map.GetLength(0); i++)
-        {
-            if (i == playerPos.X && j == playerPos.Y)
-            {
-                Console.Write(PLAYER);
-            }
-            else
-            {
-                Console.Write(map[i, j]);
-            }
-        }
-        Console.WriteLine();
-    }
-}
 
-void ImportBoard(out char[,] map)
-{
-    string[] lines = File.ReadAllLines(MAP_PATH);
-    int height = lines.Length;
-    int width = lines[0].Length;
-    map = new char[width, height];
-    for (int i = 0; i < height; i++)
-    {
-        char[] linia = lines[i].ToCharArray();
-        for (int j = 0; j < width; j++)
-        {
-            map[j, i] = linia[j];
-        }
-    }
-}
-
-struct Position
-{
-    public int X;
-    public int Y;
-};
